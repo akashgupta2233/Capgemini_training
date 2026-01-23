22971d1816a8b432507df9593e634d338d97fb26
Akash Gupta <130357593+akashgupta2233@users.noreply.github.com>
2026-01-23 12:54:05 +0530

    Add CharacterCounter class to count character occurrences

--- Q19.cs
+++ Q19.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
 
+public class CharacterCounter
+{
+    public static Dictionary<char, int> CountChars(string input)
+    {
+        var counts = new Dictionary<char, int>();
+
+        if (string.IsNullOrEmpty(input))
+            return counts;
+
+        foreach (char c in input)
+        {
+            if (counts.ContainsKey(c))
+            {
+                counts[c]++;
+            }
+            else
+            {
+                counts[c] = 1;
+            }
+        }
+
+        return counts;
+    }
+}
